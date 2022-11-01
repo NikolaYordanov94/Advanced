a855f4242b3e9d45a7b9a7658bc387742d71b54d
Nikola Yordanov <106193855+NikolaYordanov94@users.noreply.github.com>
2022-11-02 00:32:12 +0200

    Add files via upload

---
+++ Multidimensional Arrays/Lab/FindTheRealQueen_07.java
@@ -0,0 +1,11 @@
+package MultidimensionalArrays;
+
+import java.util.Scanner;
+
+public class FindTheRealQueen_07 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
