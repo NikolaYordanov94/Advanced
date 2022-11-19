460c92649a607b1027e784c4d1971de7a6d50dff
Nikola Yordanov <106193855+NikolaYordanov94@users.noreply.github.com>
2022-11-19 23:10:54 +0200

    Add files via upload

---
+++ Streams, Files and Directories/Lab/WriteToFile_02.java
@@ -0,0 +1,36 @@
+package StreamsFilesAndDirectories;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+import java.util.Set;
+
+public class WriteToFile_02 {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+
+        String path = "C:\\Users\\Nikola\\Desktop\\Java Projects\\Advanced-Java-September-2022\\resources\\input.txt";
+
+        FileInputStream fileInputStream = new FileInputStream(path);
+
+        FileOutputStream fileOutputStream = new FileOutputStream("output.txt");
+        PrintWriter printer = new PrintWriter(fileOutputStream);
+        int bytes = fileInputStream.read();
+
+        Set<Integer> delimiterTable = Set.of(10, 32);
+
+        while (bytes != -1) {
+
+            boolean isDelimiter = delimiterTable.contains(bytes);
+
+            if(isDelimiter){
+                printer.print((char) bytes);
+            }else{
+               printer.print(bytes);
+            }
+            bytes = fileInputStream.read();
+        }
+    }
+}
