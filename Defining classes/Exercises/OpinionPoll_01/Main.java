182cda07f0079bb651819265a4925559eb66687d
Nikola Yordanov <106193855+NikolaYordanov94@users.noreply.github.com>
2023-01-03 00:31:43 +0200

    Add files via upload

---
+++ Defining classes/Exercises/OpinionPoll_01/Main.java
@@ -0,0 +1,27 @@
+package DefiningClassesExercise.OpinionPoll_01;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int numberOfPeople = Integer.parseInt(scanner.nextLine());
+
+        List<Person> people = new ArrayList<>();
+
+        for (int i = 0; i < numberOfPeople; i++) {
+            String[] personData = scanner.nextLine().split(" ");
+            String firstName = personData[0];
+            int age = Integer.parseInt(personData[1]);
+
+            Person person = new Person(firstName, age);
+            people.add(person);
+        }
+        people.stream()
+                .filter(person -> person.getAge() > 30)
+                .sorted((left, right) -> left.getName().compareTo(right.getName()))
+                .forEach(person -> System.out.printf("%s - %d%n", person.getName(), person.getAge()));
+    }
+}
