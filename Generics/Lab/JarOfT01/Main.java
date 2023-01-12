83d4a2b3517ec12f8bb2e8f5dd557ca602c1d391
Nikola Yordanov <106193855+NikolaYordanov94@users.noreply.github.com>
2023-01-12 18:35:35 +0200

    Add files via upload

---
+++ Generics/Lab/JarOfT01/Main.java
@@ -0,0 +1,11 @@
+package Generics.JarOfT01;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Jar<String> stringJar = new Jar<>();
+
+        stringJar.add("Pesho");
+        stringJar.add("Ivan");
+    }
+}
