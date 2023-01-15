36547c48ca5acadfcdf692691b831293afc4b112
Nikola Yordanov <106193855+NikolaYordanov94@users.noreply.github.com>
2023-01-15 09:36:05 +0200

    Add files via upload

---
+++ Generics/Exercise/GenericSwapMethodInteger_04/Box.java
@@ -0,0 +1,39 @@
+package GenericsExercise.GenericSwapMethodInteger_04;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Box <T>{
+    private List<T> values;
+
+    public List<T> getValues() {
+        return values;
+    }
+
+    public void setValues(List<T> values) {
+        this.values = values;
+    }
+
+    public Box(){
+        this.values = new ArrayList<>();
+    }
+    public void add(T element){
+        values.add(element);
+    }
+
+    public void swap(int firstIndex, int secondIndex){
+        T temp = values.get(firstIndex);
+        values.set(firstIndex, values.get(secondIndex));
+        values.set(secondIndex, temp);
+    }
+
+    @Override
+    public String toString(){
+
+        StringBuilder sb = new StringBuilder();
+        for (T value : values){
+            sb.append(String.format("%s: %s%n", value.getClass().getName(), value.toString()));
+        }
+        return sb.toString();
+    }
+}
---
+++ Generics/Exercise/GenericSwapMethodInteger_04/Main.java
@@ -0,0 +1,22 @@
+package GenericsExercise.GenericSwapMethodInteger_04;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+        Box<Integer> box = new Box<>();
+
+        for (int i = 0; i < n; i++) {
+            int element = Integer.parseInt(scanner.nextLine());
+            box.add(element);
+        }
+        String[] indices = scanner.nextLine().split(" ");
+        int firstIndex = Integer.parseInt(indices[0]);
+        int secondIndex = Integer.parseInt(indices[1]);
+        box.swap(firstIndex, secondIndex);
+        System.out.println(box);
+    }
+}
