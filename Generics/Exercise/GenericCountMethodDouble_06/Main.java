3821c15df0973eb25a315ee2e84849fae59b9725
Nikola Yordanov <106193855+NikolaYordanov94@users.noreply.github.com>
2023-01-16 22:29:23 +0200

    Add files via upload

---
+++ Generics/Exercise/GenericCountMethodDouble_06/Main.java
@@ -0,0 +1,20 @@
+package GenericsExercise.GenericCountMethodDouble_06;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+
+        Box<Double> box = new Box<>();
+
+        for (int i = 0; i < n; i++) {
+            Double element = Double.parseDouble(scanner.nextLine());
+            box.add(element);
+        }
+
+        Double elementToCompare = Double.parseDouble(scanner.nextLine());
+        System.out.println(box.countOfGreaterItems(elementToCompare));
+    }
+}
