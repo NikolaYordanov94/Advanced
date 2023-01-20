9a1480dd418ea04714e1e1c7b250506a6e7890d3
Nikola Yordanov <106193855+NikolaYordanov94@users.noreply.github.com>
2023-01-20 21:49:15 +0200

    Add files via upload

---
+++ Iterators and Comparators/Lab/BookComparator.java
@@ -0,0 +1,10 @@
+package IteratorsAndComparators;
+import java.util.Comparator;
+
+public class BookComparator implements Comparator<Book> {
+
+    @Override
+    public int compare(Book f, Book s) {
+        return f.compareTo(s);
+    }
+}
---
+++ Iterators and Comparators/Lab/BookComparatorByYear.java
@@ -0,0 +1,11 @@
+package IteratorsAndComparators;
+import java.util.Comparator;
+
+public class BookComparatorByYear implements Comparator<Book> {
+
+    @Override
+    public int compare(Book f, Book s) {
+        return Integer.compare(f.getYear(), s.getYear());
+    }
+
+}
